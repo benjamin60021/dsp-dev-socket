ca9c9134b0dbeedb664c580745c62aad23ad61ce
benjamin60021 <71390747+benjamin60021@users.noreply.github.com>
2024-06-18 12:00:30 +0200

    Etape 4

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require("express")
+const { createServer } = require("node:http")
 const { join } = require("node:path")
-const app = express()
+const { Server } = require("socket.io")
 
-app.use("/node_modules", express.static(join(__dirname, "node_modules")))
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+app.use(express.static(join(__dirname, "utils")))
+app.use(express.static(join(__dirname, "node_modules/bootstrap/dist")))
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"))
 })
 
-app.listen(3000)
-console.log("http://localhost:3000")
+io.on("connection", (socket) => {
+  console.log("a user connected")
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected")
+  })
+
+  socket.on("STyping", () => {
+    socket.broadcast.emit("CTyping", "Quelqu'un est en train d'écrire...")
+  })
+
+  socket.on("SStopTyping", () => {
+    socket.broadcast.emit("CStopTyping")
+  })
+
+  socket.on("SMessage", (message) => {
+    io.emit("CMessage", { msg: message, id: socket.id })
+  })
+
+  socket.on("SMessageSeen", (messageId) => {
+    io.emit("CMessageSeen", { messageId, senderId: socket.id })
+  })
+})
+
+server.listen(5000, () => {
+  console.log("server running at http://localhost:5000")
+})
